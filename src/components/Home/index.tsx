5a1a8507753fd6f30d46582d00d9bcd0eb34026b
gaoliver <gabriel.o.c.ramos@gmail.com>
2021-05-21 13:36:27 -0300

    Logo effect dark-mode

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,17 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Switch from 'react-switch';
 import { ThemeContext } from 'styled-components';
 
 import './index.scss';
-import logo from '../../assets/images/GabrielRamos-logos_transparent.png';
+import lightLogo from '../../assets/images/GabrielRamos-logos_transparent.png';
+import darkLogo from '../../assets/images/GabrielRamos-logos_Background.png';
 import { App } from '../../interfaces';
 import { Container } from '../../styles/pages';
 
 const Home: React.FC<App> = ({ onSwitch }) => {
   const { colors, title } = useContext(ThemeContext);
+
+  const [logo, setLogo] = useState('');
+
+  useEffect(() => {
+    if (title === 'dark') {
+      document.getElementById("logo_img")?.classList.add("logo_img_dark")
+      setLogo(darkLogo);
+    } else {
+      document.getElementById("logo_img")?.classList.remove("logo_img_dark")
+      setLogo(lightLogo);
+    }
+  }, [title]);
   return (
     <Container id="home">
-      <img src={logo} style={{ width: 300 }} alt="Gabriel Ramos logo" />
+      <img src={logo} className="logo_img" id="logo_img" style={{ width: 300 }} alt="Gabriel Ramos logo" />
       <h1 id="typping">Hello world!</h1>
       <Switch
         onChange={onSwitch}
