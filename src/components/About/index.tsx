c40dee742c1fe91266357c588cf42063fa621d6d
gaoliver <gabriel.o.c.ramos@gmail.com>
2021-05-25 11:16:32 -0300

    Grammar

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -12,52 +12,51 @@ const About: React.FC = () => {
         <p>My name is Gabriel Ramos 👨🏽‍💻. I am Brazilian 🇧🇷.</p>
         {/* The text body */}
         <p>
-          I started programming when I was 14 years-old. When I won my first
-          computer, it was something really impressive to me. While my friends
-          were getting amazed by social networks and games, I was amazed by
-          trying to understand how were those done.
+          I started programming when I was 14 years-old. When I received my
+          first computer, it was something really impressive to me. While my
+          friends were getting amazed by social networks and games, I was amazed
+          by trying to understand how were those things done.
         </p>
         <p>
-          I still remember my first Informatics Teacher. After tottaly basic
-          classes, learning only how to use office packages, I asked her how to
-          create a website. And she got surprise for a 10 years-old boy asking
-          for learning more about something not necessary for the class. Then I
-          got a time with her, alone in the lab, to study the basic of HTML and
-          CSS. That was my very kickstart.
+          I still remember my first Informatics Teacher. After totaly basic
+          classes, learning only how to use Microsoft Office package, I asked
+          her how to create a website. And she got surprised for a 10 years-old
+          boy asking for learning more about something not necessary for the
+          class. Then I got an afterclass time with her, alone in the lab, to
+          study the basics of HTML and CSS. That was my very kickstart.
         </p>
         <p>
           In my 14{"'"}s, I was already coding my own websites and learning more
-          of PHP and JavaScript. So, in High School, I coded NerdLine website,
+          of PHP and JavaScript. So, in High School, I codded NerdLine website,
           an online newspaper about pop culture, comic books, movies and games,
           where I used to work on with my friends. The project had a
           administration portal, for creating the posts. I was the responsible
-          for all the front-end and back-end coding. The only coder of the
+          for all the front-end and back-end development. The only coder of the
           entire project. Some time later, I started using Wordpress.
         </p>
         <p>
           Since that time, I{"'"}m learning always more about programming, new
           languages and frameworks, like React and React Native (which ones I
-          use the most today), and also C#.NET (which I have used in some big
+          use the most today), also C#.NET (which I have used in some big
           important projects for companies, and for learning) and Python (for
           learning and creating data projects in the University, when I was
-          greaduating in Physics.)
+          graduating in Physics.)
         </p>
         <p>
-          During the time I was studying Physics at the University, I was part
-          of Rio de Janeiro Rocket Team, an extracurricular scientific group for
-          building rockets for national and international competition. Working
-          on it, I was developer of payload system, in Python. We got #1 in Best
-          Rocket category, in Brazil, and #3 in Best Solid Motor Rocket, at
+          During this time, studying Physics at the University, I was part of
+          Rio de Janeiro Rocket Team, an extracurricular scientific group for
+          building rockets for national and international competitions. Working
+          on it, I was the developer of payload systems, in Python. We got #1 as
+          Best Rocket, in Brazil, and #3 as Best Solid Motor Rocket, at
           Spaceport American Cup, in the USA.
         </p>
         <p>
-          Now I{"'"}m a front-end developer, expoerience on being the main
-          front-end of many web and mobile projects, based all my experience
-          focused on that position, which I enjoy the most and get better every
-          day. The achievement of my professional life is staying ahead of many
-          great and important projects and being recognized in programming field
-          as an important developer, for my knowledge and the results of my
-          jobs.
+          Now I{"'"}m a front-end developer, experienced on being the main
+          front-end of many web and mobile projects. I based all my experience
+          focused on that position, which I enjoy the most and improve myself
+          every day. The achievement of my professional life is staying ahead of
+          many important projects and being known in development field as an
+          important developer, for my knowledge and the results of my jobs.
         </p>
         <p>You can download my resumé (C.V.) in the end of the page.</p>
         {/* My professional profile area */}
