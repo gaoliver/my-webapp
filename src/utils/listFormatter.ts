0533da8e33bae08769188df90d1c816007ed3cba
Gabriel Ramos <gabriel.o.c.ramos@gmail.com>
2022-02-25 18:01:15 -0300

    feat: Description of function listFormatter

--- src/utils/listFormatter.ts
+++ src/utils/listFormatter.ts
@@ -1,3 +1,9 @@
+/**
+ * Returns a list in text format.
+ * @param list - It must be an array of strings.
+ * @example handleList(["A", "B", "C"]) //Returns "A, B and C"
+ * @author Gabriel Ramos
+ */
 function handleList(list: Array<string>) {
   return list.map((word) => {
     if (word === list[list.length - 1]) {
