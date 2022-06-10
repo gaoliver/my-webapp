f252741e37d6dc3fe1f5accd2cddb8057fc51c9a
Gabriel Ramos <gabriel.o.c.ramos@gmail.com>
2022-06-10 15:54:36 +0200

    Added props to Button

--- src/components/_desktop/_atoms/Button.tsx
+++ src/components/_desktop/_atoms/Button.tsx
@@ -6,20 +6,18 @@ import styled from 'styled-components';
 export type ButtonProps = {
   label: string;
   color?: string;
-  fontColor?: string;
-};
+} & HTMLAttributes<HTMLButtonElement>;
 
-const ButtonWrapper = styled.button<
-  HTMLAttributes<HTMLButtonElement> & { fontColor?: string }
->`
+const ButtonWrapper = styled.button`
   padding: 15px 30px;
   background-color: ${(props) => props.color || props.theme.primary};
-  color: ${props => props.fontColor || colors.white};
+  color: ${colors.white};
   border-radius: 5px;
   border: none;
   transition: ease-in-out 0.1s;
   box-shadow: 0px 0px 2px 1px ${colors.black};
   ${fonts.button}
+
   :active {
     filter: brightness(0.7);
     box-shadow: none;
