1efd693e28dff36003c94a3decb58d1af2c09c74
Gabriel Ramos <gabriel.o.c.ramos@gmail.com>
2022-06-10 14:33:08 +0200

    Button component improvement

--- src/components/_desktop/_atoms/Button.tsx
+++ src/components/_desktop/_atoms/Button.tsx
@@ -6,17 +6,18 @@ import styled from 'styled-components';
 export type ButtonProps = {
   label: string;
   color?: string;
+  fontColor?: string;
 };
 
 const ButtonWrapper = styled.button`
   padding: 15px 30px;
-  background-color: ${(props) => props.theme.primary};
+  background-color: ${(props) => props.color || props.theme.primary};
   color: ${colors.white};
-  ${fonts.button}
   border-radius: 5px;
   border: none;
   transition: ease-in-out 0.1s;
   box-shadow: 0px 0px 2px 1px ${colors.black};
+  ${fonts.button}
   :active {
     filter: brightness(0.7);
     box-shadow: none;
